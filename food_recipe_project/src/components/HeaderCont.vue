abd86fb084db7a858fb10b0c722a60f9bda406cb
KIM MINSEOK <mindory144@gmail.com>
2025-12-12 17:01:39 +0900

    2025-12-12 vuex 푸드 페이징, 게시판 목록,상세보기

--- food_recipe_project/src/components/HeaderCont.vue
+++ food_recipe_project/src/components/HeaderCont.vue
@@ -2,27 +2,18 @@
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#">Thymeleaf Include</a>
+                <router-link to="/vue/main" class="navbar-brand">Vuex Router</router-link>
             </div>
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><router-link to="/vue/main">Home</router-link></li>
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">맛집
                         <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">맛집 검색</a></li>
-                        <li><a href="#">음식별 검색</a></li>
+                        <li><router-link to="/vue/find">맛집 검색</router-link></li>
                     </ul>
                 </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">레시피
-                        <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">레시피 목록</a></li>
-                        <li><a href="#">레시피 검색</a></li>
-                        <li><a href="#">쉐프 검색</a></li>
-                    </ul>
-                </li>
+                <li><router-link to="/board/list">커뮤니티</router-link></li>
             </ul>
         </div>
     </nav>
